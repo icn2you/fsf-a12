1671ff7433d5a07a15d0927dba80d136224e6086
Christopher Zenner <christopher.zenner@gmail.com>
2020-05-18 14:59:23 -0700

    feat: add database schema and seed data

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+-- burgers_db schema
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
+  date_added DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- burgers_db seed data
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ('Chili Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Pulled Pork');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Chicken');
