83994d59bc7f3d16f3bdf4a64b145b6cf112d801
Christopher Zenner <christopher.zenner@gmail.com>
2020-05-18 20:12:49 -0700

    feat: add orm module

---
+++ config/orm.js
@@ -0,0 +1,48 @@
+const conn = require('./connection');
+
+module.exports = (() => {
+  const orm = {
+    conn: conn,
+
+    selectAll: (table, cb) => {
+      const sqlQuery = `SELECT * FROM ${table};`;
+
+      // DEBUG:
+      console.log(`sqlQuery = ${sqlQuery}`);
+
+      conn.query(sqlQuery, (err, res) => {
+        if (err) { throw err; }
+
+        cb(res);
+      });
+    },
+
+    insertOne: (table, col, val, cb) => {
+      const sqlQuery = `INSERT INTO ${table} (${col}) VALUES (${val});` 
+
+      // DEBUG:
+      console.log(`sqlQuery = ${sqlQuery}`);
+
+      conn.query(sqlQuery, (err, res) => {
+        if (err) { throw err; }
+
+        cb(res);
+      });
+    },
+
+    updateOne: (table, col, val, filter, cb) => {
+      const sqlQuery = `UPDATE ${table} SET ${col} = ${val} WHERE id = ${filter};`;
+
+      // DEBUG:
+      console.log(`sqlQuery = ${sqlQuery}`);
+
+      conn.query(sqlQuery, (err, res) => {
+        if (err) { throw err; }
+
+        cb(res);
+      });
+    }
+  };
+
+  return orm;
+})();
