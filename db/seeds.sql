edcf533ed9a54253aaba491437a0ccc1ca2fed7b
Christopher Zenner <christopher.zenner@gmail.com>
2020-05-22 20:14:49 -0700

    chore: add a few more seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,5 @@ USE burgers_db;
 INSERT INTO burgers (burger_name) VALUES ('Chili Cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('BBQ Pulled Pork');
 INSERT INTO burgers (burger_name) VALUES ('Spicy Chicken');
+INSERT INTO burgers (burger_name) VALUES ('Classic Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Peanut Butter & Bacon');
